7579c29a3d1d6ea8597b11d982787b1f469e04f2
Rouzip <1226015390@qq.com>
2017-05-23 21:34:26 +0800

    加入多位运算

--- datastruct.py
+++ datastruct.py
@@ -11,38 +11,76 @@ operate = {
     '-': 5
 }
 
+'''
+进行初步的检测
+此函数用来检测输入是否合法，如果不合法返回False
+目前想到的只有两个相连的都是运算符
+'''
+
+
+def checkInput(inputDeque)->bool:
+    global operate
+    # 左右两括号的数量
+    left, right = 0, 0
+    if not inputDeque:
+        return False
+    for pos, i in enumerate(inputDeque):
+        if pos+1 < len(inputDeque) \
+                and (i in operate and inputDeque[pos+1] in operate):
+            return False
+    # 对于括号的数目进行检测匹配
+    for i in inputDeque:
+        if i == '(':
+            left += 1
+        elif i == ')':
+            right += 1
+    if left != right:
+        return False
+    return True
+
 
 def getPostfix(inputDeque: deque)->deque:
     global operate
     # 使用stack来储存运算符号
     # num用来储存数字
+    if not checkInput(inputDeque):
+        yield Exception
     stack = deque()
     result = deque()
-
-    for i in inputDeque:
-        if '0' <= i <= '9':
-            result.append(i)
-        elif i == '(':
-            stack.append(i)
-        elif i in operate:
-            if not stack or operate[i] >= operate[stack[-1]]:
-                stack.append(i)
+    temp = ''
+    try:
+        for i in inputDeque:
+            if '0' <= i <= '9':
+                temp = ''.join(i)
                 continue
-            while stack and operate[stack[-1]] >= operate[i]\
-                    and stack[-1] != '(':
+            result.append(temp)
+            temp = ''
+            if i == '(':
+                stack.append(i)
+            elif i in operate:
+                if not stack or operate[i] >= operate[stack[-1]]:
+                    stack.append(i)
+                    continue
+                while stack and operate[stack[-1]] >= operate[i]\
+                        and stack[-1] != '(':
+                    result.append(stack.pop())
+                stack.append(i)
+            elif i == ')':
+                if not '(' in stack:
+                    yield print('您输入的括号无法匹配')
+                while stack[-1] != '(':
+                    result.append(stack.pop())
+                stack.pop()
+        else:
+            while stack:
                 result.append(stack.pop())
-            stack.append(i)
-        elif i == ')':
-            while stack[-1] != '(':
-                result.append(stack.pop())
-            stack.pop()
-    else:
-        while stack:
-            result.append(stack.pop())
-    return result
+        return result
+    except Exception as e:
+        raise e
 
 
 # 计算后缀表达式的值
+# 返回的值只可以在一定范围之内，可以考虑大数如何办，还有高精度小数
 def calculator(inputDeque: deque)->float:
     temp = deque()
     result = deque()
@@ -63,22 +101,6 @@ def calculator(inputDeque: deque)->float:
                 logging.exception(e)
 
     return int(result[0])
-
-'''
-此函数用来检测输入是否合法，如果不合法返回False
-目前想到的只有两个相连的都是运算符
-'''
-
-
-def checkInput(inputDeque)->bool:
-    global operate
-    if not checkInput:
-        return
-    for pos, i in enumerate(inputDeque):
-        if i+1 < len(inputDeque) \
-                and (i in operate and inputDeque[pos+1] in operate):
-            return False
-    return True
 
 
 if __name__ == '__main__':
